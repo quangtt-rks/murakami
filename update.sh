3eb033383df20e8a0d42c81118fc2831ea354ad7
quangtt <quangtt@rikkeisoft.com>
2015-10-02 14:04:37 +0700

    Quiet git commands

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ current_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 echo "Current version: $current_version.\n"
 
 echo "Searching for updates...\n"
-git fetch --tags origin
+git fetch --tags origin -q
 latest_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
 if [ "$current_version" = "$latest_version" ]
@@ -14,7 +14,7 @@ then
   echo "\n✓ You already have the latest version."
 else
   echo "\nUpdating $current_version to $latest_version..."
-  git merge "$latest_version"
+  git merge "$latest_version" -q
   echo "\n✓ Murakami has been updated! Have fun!"
 fi
 
