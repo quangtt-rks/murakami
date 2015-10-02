d6b85518c41ff2a365695d1a2896fac9f787cd88
quangtt <quangtt@rikkeisoft.com>
2015-10-02 11:53:36 +0700

    Update script

--- update.sh
+++ update.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-echo "\n/******************** [MURAKAMI] ********************/\n"
+echo "\n/*************** [MURAKAMI] ***************/\n"
 
-current_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-echo "✓ Current version: $current_version."
+current_tag=$(git rev-list --tags --max-count=1)
+current_version=$(git describe --tags "$current_tag")
+echo "Current version: $current_version."
 
-echo "✓ Searching for updates..."
+echo "Searching for updates..."
 git fetch --tags origin
-latest_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+latest_tag=$(git rev-list --tags --max-count=1)
+latest_version=$(git describe --tags "$latest_tag")
 
-if [ $current_version == $latest_version ]
+if [ $current_tag == $latest_tag ]
 then
-  echo "\n✓ You already have the latest version."
+  echo "\nYou already have the latest version."
 else
-  echo "\n✓ Updating $current_version to $latest_version..."
-  git merge "$latest_version"
-  echo "\n✓✓ Murakami has been updated!"
+  echo "\nUpdating $current_version to $latest_version..."
+  git merge "$latest_tag"
+  echo "\n✓ Murakami has been updated!"
 fi
 
-echo "\n******************** [MURAKAMI] ********************\n"
+echo "\n/*************** [MURAKAMI] ***************/\n"
