3a316990d9587ff061fc80a7c9e4a6688e551cc4
quangtt <quangtt@rikkeisoft.com>
2015-10-02 14:23:45 +0700

    Styling update script

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-echo "\n/*************** [MURAKAMI] ***************/\n"
+echo "\n**************** [MURAKAMI] ****************\n*"
 
+line_head="\n*  "
 current_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-echo "Current version: $current_version.\n"
+echo "$line_head Current version: $current_version.\n"
 
-echo "Searching for updates...\n"
+echo "$line_head Searching for updates..."
 git fetch --tags origin -q
 latest_version=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
 if [ "$current_version" = "$latest_version" ]
 then
-  echo "\n✓ You already have the latest version."
+  echo "\n* ✓ You already have the latest version."
 else
-  echo "\nUpdating $current_version to $latest_version..."
+  echo "$line_head Updating $current_version to $latest_version..."
   git merge "$latest_version" -q
-  echo "\n✓ Murakami has been updated! Have fun!"
+  echo "\n* ✓ Murakami has been updated! Have fun!"
 fi
 
-echo "\n/*************** [MURAKAMI] ***************/\n"
+echo "\n*\n**************** [MURAKAMI] ****************\n"
